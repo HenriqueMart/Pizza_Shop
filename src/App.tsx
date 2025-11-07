6777bdbb92c08c751755ab960da4fd10f62c14c3
henriquemart <h.martins.silva2014@gmail.com>
2025-11-07 12:17:23 -0300

    Feat: setting struct and install shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css';
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+
+  return (
+    <>
+     <div className="flex justify-center items-center h-screen">
+    <Button>Enviar</Button>
+     </div>
+    </>
+  )
+}
+
+

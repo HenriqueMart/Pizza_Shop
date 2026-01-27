67c0b2e570fd2d1ee8d1e5fa1db04624486a7277
henriquemart <h.martins.silva2014@gmail.com>
2026-01-27 09:14:49 -0300

    Feature: add page 404

--- src/route.tsx
+++ src/route.tsx
@@ -5,11 +5,13 @@ import { AuthLayout } from './components/page/_layouts/auth';
 import { SignUp } from './components/page/auth/sign-up';
 import { Orders } from './components/page/app/orders/order';
 import Dashboard from './components/page/app/dashboard/dashboard';
+import { Notfound } from './components/page/404';
 
 export const routes = createBrowserRouter([
     {
         path: '/', 
         element: <AppLayout/>,
+        errorElement: <Notfound/>,
         children: [
            {path: '/', element: <Dashboard/>}, //Conseguimos colocar como layout e page que eserá renderizada
            {path: '/orders', element: <Orders/>}

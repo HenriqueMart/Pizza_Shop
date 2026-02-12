be540b82504fa7c747465f00834c2f7ab5a91f58
henriquemart <h.martins.silva2014@gmail.com>
2026-02-12 15:30:36 -0300

    Feat: create requesting get profile and manangerRestaurant

--- src/components/ui/account-menu.tsx
+++ src/components/ui/account-menu.tsx
@@ -3,20 +3,32 @@ import { Button } from "./button";
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { DropdownMenuItem } from "./dropdown-menu";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagerRestaurant } from "@/api/get-manager-restaurant";
 
 export function AccountMenu(){
+    const { data: profile } = useQuery({
+        queryKey: ['profile'], // Definindo a identificação para essa chamada, se caso ocorrer novamente, rele pegar com base nesse key
+        queryFn: getProfile,
+    })
+    const { data: managedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'], // Definindo a identificação para essa chamada, se caso ocorrer novamente, rele pegar com base nesse key
+        queryFn: getManagerRestaurant,
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant={"outline"} className="flex items-center gap-2 select-none">
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDown className="h-4 w-4"/>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className=" flex flex-col w-56 bg-muted p-2 mt-2 border gap-2 rounded-md shadow-md">
                <DropdownMenuLabel className="flex flex-col gap-2">
-                    <span className="">Henrique Martins</span>
-                     <span className="text-xs font-normal text-muted-foreground">Henrique@gmail.com</span>
+                    <span className="">{profile?.name}</span>
+                     <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
                </DropdownMenuLabel>
                 <DropdownMenuItem>
                    

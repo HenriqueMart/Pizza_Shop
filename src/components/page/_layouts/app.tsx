910563fccc8e1b2b3392e4d94c1a17a681cc4d1f
henriquemart <h.martins.silva2014@gmail.com>
2026-04-16 09:40:57 -0300

    Feat: Error redirecionation for login

--- src/components/page/_layouts/app.tsx
+++ src/components/page/_layouts/app.tsx
@@ -1,7 +1,34 @@
 import { Header } from "@/components/header";
-import { Outlet } from "react-router-dom"; //Elemento que vai ser renderizado de cada página.
+import { api } from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom"; //Elemento que vai ser renderizado de cada página.
 
 export function AppLayout(){
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interceptorId = api.interceptors.response.use(
+            (response) => response,
+            (error) => {
+                if(isAxiosError(error)){
+                    const status = error.response?.status
+                    const code = error.response?.data?.code
+
+                    if(status === 401 && code === 'UNAUTHORIZED'){
+                        navigate('/sign-in', {replace: true})
+                    }
+                }
+            },
+        )
+
+        //Ele vai remover o interceptor quando o componente for desmontado, para evitar que ele fique ativo mesmo quando não for necessário.
+        return () => {
+            api.interceptors.response.eject(interceptorId)
+        }
+
+    }, [navigate])
+
     return (
         <div className="flex min-h-screen flex-col antialiased">
             <Header/>
